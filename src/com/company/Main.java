eafcd75a66bb39e4cb02f756d518adac7a313c9c
jh3618-kim <jh3618.kim@samsung.com>
2021-02-15 16:05:59 +0900

    Factory Method 패턴 수정

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,10 +1,10 @@
 package com.company;
 
-import com.company.cupdispenser.creator.concretecreator.SamsungCupDispenserFactory;
+import com.company.cupdispenser.creator.CupDispenserFactory;
 import com.company.cupdispenser.product.CupDispenser;
 
 public class Main {
     public static void main(String[] args) {
-        CupDispenser samsungCupDispenser = new SamsungCupDispenserFactory().createCupDispenser();
+        CupDispenser samsungCupDispenser = CupDispenserFactory.createCupDispenser(VendorID.SAMSUNG);
     }
 }
--- src/com/company/cupdispenser/creator/CupDispenserFactory.java
+++ src/com/company/cupdispenser/creator/CupDispenserFactory.java
@@ -1,7 +1,18 @@
 package com.company.cupdispenser.creator;
 
+import com.company.VendorID;
 import com.company.cupdispenser.product.CupDispenser;
+import com.company.cupdispenser.product.concreteproduct.NespressoCupDispenser;
+import com.company.cupdispenser.product.concreteproduct.SamsungCupDispenser;
 
-public abstract class CupDispenserFactory {
-    public abstract CupDispenser createCupDispenser();
+public class CupDispenserFactory {
+    public static CupDispenser createCupDispenser(VendorID vendorID) {
+        switch (vendorID) {
+            case SAMSUNG:
+                return new SamsungCupDispenser();
+            case NESPRESSO:
+                return new NespressoCupDispenser();
+        }
+        return null;
+    }
 }
